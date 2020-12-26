9955dfb02994303c4909699f284665e526caab20
Prem Kumar <PremK.LF@gmail.com>
2020-12-27 00:43:59 +0530

    02.firestore db+chakra UI (#3)
    * headline changes
    
    * auth-setup
    
    * sign-in and sign-out completed
    
    * setup: firestore db, chakra ui, auth code refactoring

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,39 @@
-import { AuthProvider } from '../lib/auth';
-import '../styles/globals.css';
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
+import { Global, css } from '@emotion/core';
+import { AuthProvider } from '@/lib/auth';
+import customTheme from '@/styles/theme';
 
-function MyApp({ Component, pageProps }) {
+const GlobalStyle = ({ children }) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
   );
-}
+};
 
-export default MyApp;
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,30 @@
+import { Button, Code, Text, Heading } from '@chakra-ui/core';
 import Head from 'next/head';
-import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
+import { useAuth } from '@/lib/auth';
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Feedback.io</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Feedback.io</h1>
+      <main>
+        <Heading>Feedback.io</Heading>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Text>
+          Current user:
+          <Code>{auth.user ? auth.user.name : 'Guest'}</Code>
+        </Text>
 
-        <button onClick={() => auth.signinWithGitHub()}>Sign In</button>
-        <button onClick={() => auth.signout()}>Sign Out</button>
-        <p>{auth?.user?.email}</p>
+        {auth.user ? (
+          <Button onClick={() => auth.signout()}>Sign Out</Button>
+        ) : (
+          <Button onClick={() => auth.signinWithGitHub()}>Sign In</Button>
+        )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
