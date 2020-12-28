e25908033b3a431f8da262143c5549935c1ffcc2
prem-io <PremK.Lf@gmail.com>
2020-12-28 23:02:01 +0530

    toast UI added, save site date to firestor, hook-up firebase server-side, API route setup to fetch sites, SWR integration

--- components/DashboardShell.jsx
+++ components/DashboardShell.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import NextLink from 'next/link';
-import { Box, Flex, Link, Avatar, Icon } from '@chakra-ui/core';
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Heading,
+  Button,
+  Flex,
+  Link,
+  Avatar,
+  Icon
+} from '@chakra-ui/core';
 
 import { useAuth } from '@/lib/auth';
-// import Footer from './Footer';
+import AddSiteModal from './AddSiteModal';
 
 const DashboardShell = ({ children }) => {
-  const { user } = useAuth();
+  const { user, signout } = useAuth();
 
   return (
     <Box backgroundColor="gray.100" h="100vh">
-      <Flex
-        backgroundColor="white"
-        mb={[8, 16]}
-        w="full"
-        borderTop="5px solid #0AF5F4"
-      >
+      <Flex backgroundColor="white" mb={16} w="full">
         <Flex
           alignItems="center"
           justifyContent="space-between"
@@ -25,34 +30,35 @@ const DashboardShell = ({ children }) => {
           margin="0 auto"
           w="full"
           px={8}
-          h="60px"
+          h="70px"
         >
-          <Flex align="center">
-            <NextLink href="/" passHref>
-              <Link>
-                <Icon name="logo" size="24px" mr={8} />
-              </Link>
-            </NextLink>
-            <NextLink href="/sites" passHref>
-              <Link mr={4}>Sites</Link>
-            </NextLink>
-            <NextLink href="/feedback" passHref>
-              <Link>Feedback</Link>
-            </NextLink>
+          <Flex>
+            <Icon name="logo" size="24px" mr={8} />
+            <Link mr={4}>Sites</Link>
+            <Link>Feedback</Link>
           </Flex>
           <Flex justifyContent="center" alignItems="center">
-            <NextLink href="/account" passHref>
-              <Link>
-                <Avatar size="sm" src={user?.photoUrl} />
-              </Link>
-            </NextLink>
+            {user && (
+              <Button variant="ghost" mr={2} onClick={() => signout()}>
+                Log Out
+              </Button>
+            )}
+            <Avatar size="sm" src={user?.photoUrl} />
           </Flex>
         </Flex>
       </Flex>
-      <Flex margin="0 auto" direction="column" maxW="1250px" px={[0, 8, 8]}>
+      <Flex margin="0 auto" direction="column" maxW="1250px" px={8}>
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <BreadcrumbLink>Sites</BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+        <Flex justifyContent="space-between">
+          <Heading mb={8}>My Sites</Heading>
+          <AddSiteModal>+ Add Site</AddSiteModal>
+        </Flex>
         {children}
       </Flex>
-      {/* <Footer /> */}
     </Box>
   );
 };
